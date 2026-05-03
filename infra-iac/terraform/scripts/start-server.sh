b682fe9b0114ddb2ca27426f9c87b059376f89b7
crazyoyo <crazyoyo@qq.com>
2026-05-03 23:03:41 +0800

    chown nomad TLS certs to nomad user before chmod

--- infra-iac/terraform/scripts/start-server.sh
+++ infra-iac/terraform/scripts/start-server.sh
@@ -40,6 +40,7 @@ mkdir -p /opt/nomad/tls
 get_secret "${NOMAD_TLS_CA_SECRET}" > /opt/nomad/tls/ca.pem
 get_secret "${NOMAD_TLS_CERT_SECRET}" > /opt/nomad/tls/cert.pem
 get_secret "${NOMAD_TLS_KEY_SECRET}" > /opt/nomad/tls/key.pem
+chown nomad:nomad /opt/nomad/tls/*.pem
 chmod 600 /opt/nomad/tls/*.pem
 
 cp /opt/nomad/tls/ca.pem /opt/consul/tls/ca/ca.pem
